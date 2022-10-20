2e4fc23da70ff5c725c14cbd49a22bfd54c782e7
karivone <karimiyvonne19@gmail.com>
2022-10-20 22:36:03 +0300

    printf  function

--- main.h
+++ main.h
@@ -58,5 +58,4 @@ char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 
-#endif
 #endif  /* MAIN_H */
