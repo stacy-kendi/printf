cc2b7e6039c8fe98d571b76eee5cb8ad15a37548
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-10-22 14:22:05 -0700

    update of main.h

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct print - struct for printer functions
@@ -13,12 +14,14 @@
  * Description: struct that stores pointers to a
  * printer functions.
  */
+
 typedef struct print
 {
-        char *type_arg;
-        int (*f)(va_list, char *, unsigned int);
+        char *t;
+        int (*f)(va_list);
 } print_t;
 
+int _putchar(char c);
 int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
@@ -57,5 +60,18 @@ char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
 #endif  /* MAIN_H */
